2850285dadff6bdc67aa6ea558321920359ae37b
TobyNguyen710 <ductoby2003@gmail.com>
2025-03-07 00:46:50 -0800

    Finished Main Menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,13 +1,23 @@
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject titleImage;
     [SerializeField] private GameObject settingsMenu;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private float startTime;
 
     void Awake()
     {
         settingsMenu.SetActive(false);
+        if (audioSource != null)
+        {
+            audioSource.time = startTime; // Set start time
+            audioSource.Play(); // Play from the specified time
+        }
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,12 +33,30 @@ public class MenuManager : MonoBehaviour
     public void OpenSettings()
     {
         mainMenu.SetActive(false);
+        titleImage.SetActive(false);
         settingsMenu.SetActive(true);
     }
     public void CloseSettings()
     {
         mainMenu.SetActive(true);
+        titleImage.SetActive(true);
         settingsMenu.SetActive(false);
+    }
+    public void SwitchScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+    public void SetVolume(float volume)
+    {
+        audioSource.volume = Mathf.Clamp01(volume); // Ensure volume stays between 0 and 1
     }
 
 }
