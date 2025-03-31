d991f3c2053df2ddd87fdedb5f5d56a8facd8957
TobyNguyen710 <ductoby2003@gmail.com>
2025-03-30 18:21:47 -0700

    [T] Last Update (nerf some maps)

--- Assets/Scripts/Logics/Map/WindFollow.cs
+++ Assets/Scripts/Logics/Map/WindFollow.cs
@@ -1,16 +1,36 @@
+using System.Collections;
 using UnityEngine;
 
 public class WindFollow : MonoBehaviour
 {
     [SerializeField] private ParticleSystem windParticleLeft;
     [SerializeField] private ParticleSystem windParticleRight;
-    [SerializeField] private Transform player;
+    [SerializeField] private Player player;
     [SerializeField] private float particleOffset = 20.0f; // Distance from player
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
+        StartCoroutine(AssignPlayerAfterDelay());
     }
+    private IEnumerator AssignPlayerAfterDelay()
+    {
+        yield return new WaitForSeconds(1);
+
+        // Find all active Player objects
+        Player[] players = FindObjectsByType<Player>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+
+        if (players.Length > 0)
+        {
+            player = players[0];
+            Debug.Log("Player from Scene 1 assigned!");
+        }
+        else
+        {
+            Debug.LogWarning("No Player instance found!");
+        }
+    }
+
 
     // Update is called once per frame
 
@@ -18,7 +38,7 @@ public class WindFollow : MonoBehaviour
     void Update()
     {
         // Make the particle systems follow the player’s position
-        windParticleLeft.transform.position = player.position + Vector3.right * particleOffset;
-        windParticleRight.transform.position = player.position - Vector3.right * particleOffset;
+        windParticleLeft.transform.position = player.transform.position + Vector3.right * particleOffset;
+        windParticleRight.transform.position = player.transform.position - Vector3.right * particleOffset;
     }
 }
